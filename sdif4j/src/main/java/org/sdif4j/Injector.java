73e7ecfe387dd7329863c85e56ddffd593914a23
seregamorph <serega.morph@gmail.com>
2014-03-24 00:37:56 +0400

    spring-sdif4j-context.xml

--- sdif4j/src/main/java/org/sdif4j/Injector.java
+++ sdif4j/src/main/java/org/sdif4j/Injector.java
@@ -6,19 +6,20 @@ import javax.inject.Provider;
 
 /**
  * Inject IOC encapsulation class.
- *
+ * <p/>
  * In guice IOC by default implemented by {@code org.sdif4j.guice.GuiceInjector}
  * (no additional binding required, auto-bound with @ImplementedBy annotation).
- *
+ * <p/>
  * In Spring IOC by default implemented by {@code org.sdif4j.spring.SpringInjector}
- * ("org.sdif4j.spring" auto-scan required).
- *
+ * ("org.sdif4j.spring" auto-scan required or simply add
+ * &lt;import resource="classpath:spring-sdif4j-context.xml"/&gt; to your context.xml).
+ * <p/>
  * In CDI IOC by default implemented by {@code org.sdif4j.cdi.CdiInjector}
  * (no additional binding required, auto-bound with @Default annotation)
- *
+ * <p/>
  * General notice: when feasible, do not use this class and it's methods,
  * use javax.inject.* annotations instead.
- *
+ * <p/>
  * Note, that @ImplementedBy is ignored by Spring and CDI IOC
  * (does not matter if Guice presents in classpath or not).
  *
